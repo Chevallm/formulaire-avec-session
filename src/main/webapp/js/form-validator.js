20d6305b0756bd5578327bd32b93be959c1b7453
Maxime CHEVALLIER-PICHON <maxime.chevallier_pichon@yahoo.fr>
2017-03-23 13:12:31 +0100

    add secondForm.html

--- src/main/webapp/js/form-validator.js
+++ src/main/webapp/js/form-validator.js
@@ -1,6 +1,10 @@
-const r = new RegExp("^[a-zA-ZÀ-ÿ- ]*$");
-
-const validate = function(formData) {
+const validate = function(formData, regexpStyle) {
+	var r;
+	if(regexpStyle == "only letters with accent") {
+		r = new RegExp("^[a-zA-ZÀ-ÿ- ]*$");
+	} else if ( regexpStyle == "adresse postale" ) {
+		r = new RegExp("[a-zA-Z 0-9, ]");
+	}
 	var isValid = false;
 	for(var input in formData) {
 		
@@ -16,7 +20,7 @@ const validate = function(formData) {
 };
 
 const isEmpty = function(toCheck) {
-	return (toCheck == "" || toCheck === "undefined")
+	return (toCheck == "" || toCheck === undefined)
 };
 
 
